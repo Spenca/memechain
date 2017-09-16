4ab0b7f7d2215377e148df5cbee1a9e9f9bb992e
Spencer Vatrt-Watts <asvw@uvic.ca>
2017-09-16 04:13:41 -0400

    Add block creation and genesis retrieval methods

--- main.js
+++ main.js
@@ -10,7 +10,24 @@ class Block {
 	}
 }
 
+var getGenesisBlock = () => {
+	return new Block(0, "0", {"title": "Rarest Pepe","image_url": "https://imgur.com/pEMGBhR"}, "a3e73f3079c231148bc55eac355bc2b0950e2ee4994b5d0bbfb95388c0dd0dfa");
+}
+
+var blockchain = [getGenesisBlock()];
+
+var getLatestBlock = () => {
+	return blockchain[blockchain.length - 1];
+}
+
 var calculateHash = (index, previousHash, data) => {
 	return CryptoJS.SHA256(index + previousHash + data).toString();
 };
+
+var makeNewBlock = (blockData) => {
+	var previousBlock = getLatestBlock();
+	var newIndex = previousBlock.index + 1;
+	var newHash = calculateHash(newIndex, previousBlock.hash, blockData);
+	return new Block(newIndex, previousBlock.hash, blockData, newHash);
+}
 
