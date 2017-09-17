a4fa53bf26429706bfdfc5e4ca2812fbe79c0bc5
Spencer Vatrt-Watts <asvw@uvic.ca>
2017-09-16 22:26:11 -0400

    Test methods

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ var p2pPort = process.env.P2P_PORT || 6001;
 var initialPeers = process.env.PEERS ? process.env.PEERS.split(",") : [];
 
 class Block {
-	constructor(index, previousHash, data, hash) {
+	constructor(index, previousHash, data, hash, author) {
 		this.index = index;
 		this.previousHash = previousHash;
 		this.data = data;
 		this.hash = hash;
+		this.author = author;
 		this.timestamp = new Date().getTime();
 	}
 }
@@ -25,7 +26,7 @@ var messageType = {
 };
 
 var getGenesisBlock = () => {
-	return new Block(0, "0", {"title": "Rarest Pepe","image_url": "https://imgur.com/pEMGBhR"}, "a3e73f3079c231148bc55eac355bc2b0950e2ee4994b5d0bbfb95388c0dd0dfa");
+	return new Block(0, "0", {"title": "Rarest Pepe","image_url": "https://imgur.com/pEMGBhR"}, "a3e73f3079c231148bc55eac355bc2b0950e2ee4994b5d0bbfb95388c0dd0dfa", "James and Spencer");
 };
 
 var blockchain = [getGenesisBlock()];
@@ -34,19 +35,19 @@ var getLatestBlock = () => {
 	return blockchain[blockchain.length - 1];
 };
 
-var calculateHash = (index, previousHash, data) => {
-	return CryptoJS.SHA256(index + previousHash + data).toString();
+var calculateHash = (index, previousHash, data, author) => {
+	return CryptoJS.SHA256(index + previousHash + data + author).toString();
 };
 
 var calculateHashForBlock = (block) => {
-	return calculateHash(block.index, block.previousHash, block.data);
+	return calculateHash(block.index, block.previousHash, block.data, block.author);
 };
 
-var makeNewBlock = (blockData) => {
+var makeNewBlock = (blockData, blockAuthor) => {
 	var previousBlock = getLatestBlock();
 	var newIndex = previousBlock.index + 1;
-	var newHash = calculateHash(newIndex, previousBlock.hash, blockData);
-	return new Block(newIndex, previousBlock.hash, blockData, newHash);
+	var newHash = calculateHash(newIndex, previousBlock.hash, blockData, blockAuthor);
+	return new Block(newIndex, previousBlock.hash, blockData, newHash, blockAuthor);
 };
 
 var verifyBlock = (newBlock, previousBlock) => {
@@ -148,7 +149,7 @@ var startHTTPServer = () => {
 	app.get("/blocks", (req, res) => res.send(JSON.stringify(blockchain)));
 	
 	app.post("/mineBlock", (req, res) => {
-		var newBlock = makeNewBlock(req.body.data);
+		var newBlock = makeNewBlock(req.body.data, req.body.author);
 		addBlockToChain(newBlock);
 		broadcast(latestBlockResponse());
 		console.log("Block added: " + JSON.stringify(newBlock));
@@ -164,13 +165,20 @@ var startHTTPServer = () => {
     res.send();
   });
   
+  // app.post("/buyMeme", (req, res) => {
+  // 	var hash = req.body.hash;
+  // 	var descendingChain = blockchain.sort((b1, b2) => (b2.index - b1.index));
+  // 	descendingChain.forEach((block) => {
+  		
+  // 	});
+  // });
   app.listen(httpPort, () => console.log("Listening for HTTP on port: " + httpPort));
 };
 
 var startP2PServer = () => {
 	var server = new WebSocket.Server({port: p2pPort});
   server.on("connection", ws => startConnection(ws));
- 	console.log("Listening for websocket P2P on port: " + p2pPort);
+ 	console.log("Listening for WebSocket P2P on port: " + p2pPort);
 };
 
 var startConnection = (ws) => {
